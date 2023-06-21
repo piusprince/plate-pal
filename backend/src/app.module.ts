1a27f59d661138a5f0bee47fd97b59d025717d89
Pius Prince <45078705+piusprince@users.noreply.github.com>
2023-06-21 01:32:17 +0000

    Import PrismaModule

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -5,6 +5,7 @@ import { GraphQLModule } from '@nestjs/graphql';
 import { join } from 'path';
 import { RestaurantsModule } from './restaurants/restaurants.module';
 import { ApolloDriver } from '@nestjs/apollo';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
   imports: [
@@ -13,6 +14,7 @@ import { ApolloDriver } from '@nestjs/apollo';
       driver: ApolloDriver,
     }),
     RestaurantsModule,
+    PrismaModule,
   ],
   controllers: [],
   providers: [AppService],
