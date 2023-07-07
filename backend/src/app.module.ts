d49a1d5ae365fdf358e9d1c6424136d623f39aeb
Pius Prince <45078705+piusprince@users.noreply.github.com>
2023-07-07 17:09:14 +0000

    feat: Setup config module and joi

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -6,9 +6,21 @@ import { join } from 'path';
 import { RestaurantsModule } from './restaurants/restaurants.module';
 import { ApolloDriver } from '@nestjs/apollo';
 import { PrismaModule } from './prisma/prisma.module';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
+
+console.log(Joi);
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: process.env.NODE_ENV === 'dev' ? '.env.dev' : '.env.test',
+      ignoreEnvFile: process.env.NODE_ENV === 'prod',
+      validationSchema: Joi.object({
+        NODE_ENV: Joi.string().valid('dev', 'prod', 'test').required(),
+      }),
+    }),
     GraphQLModule.forRoot({
       autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
       driver: ApolloDriver,
