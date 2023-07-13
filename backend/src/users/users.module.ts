dd9bb7d6686638abd8b4753d96b31d4bdf15d69f
Pius Prince <45078705+piusprince@users.noreply.github.com>
2023-07-13 14:01:40 +0000

    feat: Import jwt config into user module

--- backend/src/users/users.module.ts
+++ backend/src/users/users.module.ts
@@ -2,9 +2,17 @@ import { Module } from '@nestjs/common';
 import { PrismaModule } from 'src/prisma/prisma.module';
 import { UsersResolver } from './users.resolver';
 import { UsersService } from './users.service';
+import { JwtModule } from '@nestjs/jwt';
+import jwtConfig from 'src/config/jwt.config';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [
+    PrismaModule,
+    JwtModule.registerAsync(jwtConfig.asProvider()),
+    ConfigModule.forFeature(jwtConfig),
+  ],
+
   providers: [UsersResolver, UsersService],
 })
 export class UsersModule {}
